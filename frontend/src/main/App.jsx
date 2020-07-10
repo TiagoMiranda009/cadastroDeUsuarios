1d8728ae5d008addf452b1e19f22109792899970
Tiago Miranda <tiagomiranda009@gmail.com>
2020-07-10 12:25:21 -0300

    Adicionando componentes de conteúdo

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -5,14 +5,13 @@ import React from 'react'
 
 import Logo from '../componentes/template/Logo'
 import Nav from '../componentes/template/Nav'
-import Main from '../componentes/template/Main'
+import Home from '../componentes/home/Home'
 import Footer from '../componentes/template/Footer'
 
 export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main icon="home" title="Inicio"
-            subtitle="Segundo Projeto do capitulo de React." />
+        <Home />
         <Footer />
     </div>
