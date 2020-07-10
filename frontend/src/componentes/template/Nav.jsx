a731eddbab162094a7a7f0e1e99f61492ed9df8b
Tiago Miranda <tiagomiranda009@gmail.com>
2020-07-10 12:09:05 -0300

    Adicionando componentes de navegação

--- frontend/src/componentes/template/Nav.jsx
+++ frontend/src/componentes/template/Nav.jsx
@@ -3,5 +3,13 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        <nav className="menu">
+            {/* Refatorar em casa! */}
+            <a href="#/">
+                <i className="fa fa-home"></i> Inicío
+            </a>
+            <a href="#/users">
+                <i className="fa fa-home"></i> Usuários
+            </a>
+        </nav>
     </aside>
